fc39cd079ba23559c85f59bfb513a70a3f6a5bdc
bmarques0 <brunofelipe10@hotmail.com>
2017-10-30 20:27:40 -0200

    SCRIPT criação das tabelas empregado e produto
    CREATE TABLE TB_EMPREGADO
    CREATE TABLE TB_PRODUTO

---
+++ CREATE_TABLE_EMPREGADO.sql
@@ -0,0 +1,18 @@
+CREATE TABLE tb_empregado(
+        cod_emp int NOT NULL,
+        nome_emp VARCHAR2(50) NOT NULL, 
+        tel_emp int NOT NULL,
+        sexo_emp int,
+        datanasc_emp date,
+        cfp_emp int UNIQUE,
+        rg_emp int UNIQUE,
+        mae_emp VARCHAR2(50),
+        rua_emp VARCHAR2(100),
+        cidade_emp VARCHAR2(40),
+        uf_emp VARCHAR2(5),
+        cep_emp int NOT NULL,
+        salario_emp int,
+        PRIMARY KEY (cod_emp),
+        CONSTRAINT uk_emp UNIQUE (nome_emp, mae_emp, datanasc_emp),
+        CONSTRAINT ck_pk CHECK (sexo_emp in ('M', 'F') AND salario_emp > 800)
+    );
---
+++ CREATE_TABLE_PRODUTO.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tb_produto
+(
+  cod_prod int,
+  nome_prod varchar(50),
+  valor_prod int,
+  PRIMARY KEY (cod_prod)
+);  
