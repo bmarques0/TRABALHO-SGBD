e911fa54ad187496d2bfdce72b500d5f1c54daa2
bmarques0 <brunofelipe10@hotmail.com>
2017-10-30 23:35:57 -0200

    tabela venda incompleta
    tabela venda incompleta

--- CREATE-TABLE-VENDA.sql
+++ CREATE-TABLE-VENDA.sql
@@ -1,6 +1,9 @@
 CREATE TABLE tb_venda(
+    codProdutoEmpregado int PRIMARY KEY,
+    codProduto int,
+    codEmpregado int,
     qtdade_venda INT,
     dataHora_venda DATE,
-    codProduto INT FOREIGN KEY REFERENCES tb_produto(cod_prod),
-    codEmpregado INT FOREIGN KEY REFERENCES tb_empregado(cod_emp)
+    CONSTRAINT  fk_codProd FOREIGN KEY(codProduto) REFERENCES tb_produto(cod_prod),
+    CONSTRAINT fk_codEmp FOREIGN KEY (codEmpregado) REFERENCES tb_empregado(cod_emp)
 );
